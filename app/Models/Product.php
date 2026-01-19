8d52222903783bc20a5d11f70e698dab53c34494
Wallace <wallaceb.teixeira@gmail.com>
2026-01-18 22:21:53 -0300

    chore: :hammer: create models and migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+        use HasFactory;
+
+    protected $fillable = ['name', 'brand_id'];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
